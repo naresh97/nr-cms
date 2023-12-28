cf9e1f6532c8e8a1988d8528e68d77fdf4fa2a4e
Nareshkumar Rao <inaresh.online@gmail.com>
2023-12-28 02:27:17 +0100

    add navbar generation

--- src/gen.rs
+++ src/gen.rs
@@ -13,8 +13,33 @@ fn get_title(cms_file: &CMSFile) -> &str {
     }
 }
 
+fn get_navbar(cms_file: &CMSFile) -> String {
+    let navbar = cms_file.templates.iter().find(|x| {
+        match x {
+            TemplateType::Navbar { paths } => true,
+            _ => false
+        }
+    });
+    match navbar {
+        Some(TemplateType::Navbar { paths }) => {
+            let mut links = String::new();
+            let paths: Vec<String> = paths.iter().filter_map(|x| {
+                let path_str = x.to_str();
+                match path_str {
+                    Some(path_str) => Some(format!("<a href=\"{path_str}\">{path_str}</a>")),
+                    _ => None
+                }
+            }).collect();
+            let paths = paths.join(" | ");
+            paths
+        }
+        _ => String::new()
+    }
+}
+
 pub fn generate_website(cmsfile: &CMSFile) -> String {
     let title = get_title(cmsfile);
+    let navbar = get_navbar(cmsfile);
     let site = format!(r#"
     <html>
     <head>
@@ -22,6 +47,7 @@ pub fn generate_website(cmsfile: &CMSFile) -> String {
     </head>
     <body>
     <h1>{title}</h1>
+    {navbar}
     </body>
     </html>
     "#);
