3f0adf0c73eb2fbba4c677563e579b34625c0448
Nareshkumar Rao <inaresh.online@gmail.com>
2023-12-31 01:11:49 +0100

    add more unit tests

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,22 @@ fn main() {
     if let Some(max_log_level) = &run_args.max_log_level {
         log::debug!("Max Log Level: {}", max_log_level);
     }
-    if let Err(e) = watcher::watch(run_args) {
+    if let Err(e) = watcher::watch(run_args, None) {
         log::error!("Could not initialize watcher: {e}");
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_init_logging() {
+        let run_args = run_args::RunArgs {
+            generation_dir: Default::default(),
+            source_dir: Default::default(),
+            max_log_level: Some("trace".to_string()),
+        };
+        init_logging(&run_args);
+        assert_eq!(log::max_level(), log::LevelFilter::Trace);
     }
 }
--- src/watcher.rs
+++ src/watcher.rs
@@ -1,19 +1,19 @@
+use std::sync::mpsc::Receiver;
+
 use notify::{RecursiveMode, Watcher};
 
 use crate::{generation::generate_website::load_and_write_site, run_args};
 
-fn watch_event(_event: notify::Event, run_args: run_args::RunArgs) {
-    let handled = match _event.kind {
-        notify::EventKind::Any => (false, "any"),
-        notify::EventKind::Access(_) => (false, "access"),
+fn watch_event(event: notify::Event, run_args: run_args::RunArgs) {
+    let handled = match event.kind {
         notify::EventKind::Create(_) => (true, "create"),
         notify::EventKind::Modify(_) => (true, "modify"),
         notify::EventKind::Remove(_) => (true, "remove"),
-        notify::EventKind::Other => (false, "other"),
+        _ => (false, "other"),
     };
     log::trace!(
         "Filesystem event path: {}",
-        _event
+        event
             .paths
             .iter()
             .filter_map(|x| x.to_str())
@@ -32,12 +32,93 @@ fn watch_error(e: notify::Error) {
     log::error!("Filesystem Watcher Error: {}", e.to_string());
 }
 
-pub fn watch(run_args: run_args::RunArgs) -> Result<(), notify::Error> {
+pub fn watch(
+    run_args: run_args::RunArgs,
+    cancellation_token: Option<Receiver<bool>>,
+) -> Result<(), notify::Error> {
     let source_dir = &run_args.source_dir.clone();
     let mut watcher = notify::recommended_watcher(move |res| match res {
         Ok(event) => watch_event(event, run_args.clone()),
         Err(e) => watch_error(e),
     })?;
     watcher.watch(std::path::Path::new(source_dir), RecursiveMode::Recursive)?;
-    loop {}
+    loop {
+        let cancellation_token = cancellation_token.as_ref().map(|x| x.recv().ok()).flatten();
+        if let Some(cancellation_token) = cancellation_token {
+            if cancellation_token {
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::time::Duration;
+
+    use crate::run_args::RunArgs;
+
+    use super::*;
+    #[test]
+    fn test_watch_methods() {
+        let run_args = RunArgs {
+            generation_dir: Default::default(),
+            source_dir: Default::default(),
+            max_log_level: Default::default(),
+        };
+
+        // Should not panic
+        watch_event(
+            notify::Event {
+                kind: Default::default(),
+                paths: Default::default(),
+                attrs: Default::default(),
+            },
+            run_args.clone(),
+        );
+        watch_event(
+            notify::Event {
+                kind: notify::EventKind::Modify(notify::event::ModifyKind::Data(
+                    notify::event::DataChange::Any,
+                )),
+                paths: Default::default(),
+                attrs: Default::default(),
+            },
+            run_args.clone(),
+        );
+
+        // Should not panic
+        watch_error(notify::Error {
+            kind: notify::ErrorKind::Generic(Default::default()),
+            paths: Default::default(),
+        });
+    }
+
+    #[test]
+    fn test_watch() {
+        let run_args = RunArgs {
+            generation_dir: Default::default(),
+            source_dir: Default::default(),
+            max_log_level: Default::default(),
+        };
+        let (tx, rx) = std::sync::mpsc::channel::<bool>();
+        let handle = std::thread::spawn(move || {
+            watch(run_args, Some(rx)).unwrap();
+        });
+        tx.send(false).unwrap();
+        std::thread::sleep(Duration::from_millis(100));
+        assert!(!handle.is_finished());
+        tx.send(true).unwrap();
+        for _ in 0..10 {
+            if !handle.is_finished() {
+                std::thread::sleep(Duration::from_millis(100));
+            } else {
+                handle.join().unwrap();
+                return;
+            }
+        }
+        panic!("Could not kill watcher thread");
+    }
 }
